ab193eba6fd7ed965d4849cf8a042e8d586f391d
JarryLi <3244188372@qq.com>
2019-07-11 20:08:38 +0800

    all pass and finish should_return_unrepeated_elements_from_evens

--- src/main/java/com/thoughtworks/collection/Add.java
+++ src/main/java/com/thoughtworks/collection/Add.java
@@ -44,7 +44,7 @@ public class Add {
     }
 
     public List<Integer> getUnrepeatedFromEvenIndex(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        return arrayList.stream().filter(item->item%2==0).distinct().collect(Collectors.toList());
     }
 
 //    public List<Integer> sortByEvenAndOdd(List<Integer> arrayList) {
